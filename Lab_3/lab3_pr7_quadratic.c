cbf0320b237a4cdfab67b9a62202299a941c588a
Mohammad Mesbahul Alam <127547048+Mesbahul-jigar@users.noreply.github.com>
2023-10-10 01:22:07 +0600

    Update lab3_pr7_quadratic.c

--- Lab_3/lab3_pr7_quadratic.c
+++ Lab_3/lab3_pr7_quadratic.c
@@ -9,15 +9,16 @@ scanf("%f%f%f",&a,&b,&c);
 	
 	if(D==0){ x1=x2=(-b/2*a);
 		printf("Roots of the equation are %.3f and %.3f",x1,x2);
-	        }
+	}
 	else if(D>0){ 
 	        x1=(-b+sqrt(D))/(2*a);
 	        x2=(-b-sqrt(D))/(2*a);
 	        printf("Roots of the equation are %.3f and %.3f",x1,x2);
-	        }
+	}
 	else{
 		real=(-b/2*a);
 		img=sqrt(-D)/(2*a);
-		printf("Roots of the equation are %.3f+i%.3f and %.3f-i%.3f ",real,img,real,img);}
+		printf("Roots of the equation are %.3f+i%.3f and %.3f-i%.3f ",real,img,real,img);
+	}
 return 0;	
-}}
+}
